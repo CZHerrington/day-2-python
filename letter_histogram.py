d8cd6e71c96e7d2f2dfb7b59bde61de143668038
Christopher Herrington <christopher.herrington@outlook.com>
2019-09-04 12:50:14 -0400

    I chose two sequential for loops instead of one to massively decrease the logic required during each interation

---
+++ letter_histogram.py
@@ -0,0 +1,17 @@
+# Write a letter_histogram program that asks the user for input, and prints a dictionary containing the tally of how many times each letter in the alphabet was used in the word.
+
+def histogram(word):
+    count = {}
+    
+    for letter in word:
+        if letter == '': return TypeError('word cannot contain spaces!')
+        count[letter] = 0
+
+    for letter in word:
+        count[letter] += 1
+
+    return count
+
+word = str(input('please enter a non-space-containing word to recieve letter histogram: '))
+
+print(histogram(word))
