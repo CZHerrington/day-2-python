ef8c0b308d6dfc3d12f81b935161632b57a42e3c
Christopher Herrington <christopher.herrington@outlook.com>
2019-09-04 22:07:49 -0400

    full functionality, need to clean up code

--- hotelman.py
+++ hotelman.py
@@ -1,5 +1,15 @@
-db = {}
-
+db = {
+  '1': {
+    '101': ['George Jefferson', 'Wheezy Jefferson'],
+  },
+  '2': {
+    '237': ['Jack Torrance', 'Wendy Torrance'],
+  },
+  '3': {
+    '333': ['Neo', 'Trinity', 'Morpheus']
+  }
+}
+run = True
 # db operations
 def add_guests(floor, room, occupants):
     if floor not in db:
@@ -7,23 +17,26 @@ def add_guests(floor, room, occupants):
 
     if room not in db[floor]:
         db[floor][room] = occupants
-        print("added!", db)
+        print("Your room number is %s!" % (str(room)), db)
     else:
-        Exception("Guests already checked in to that room! [this code should not be reached]")
+        print("Guests already checked in to that room!")
 
 def remove_guests(floor, room):
-    if floor not in db or room not in db[floor]:
-        Exception('No one is checked in to that room! [this code should not be reached]')
-    del db[floor][room]
+    if floor in db:
+        print('floor exists..')
+        if room in db[floor]:
+            print('room occupied...')
+            del db[floor][room]
+        print("thank you, you are checked out of room %s" % (str(floor) + str(room)))
 
 # utility fxns
 def check_room_free(floor, room):
-    if floor not in db or room not in db[floor]:
+    if str(floor) not in db or str(room) not in db[str(floor)]:
         return True
     else:
         return False
 
-# prompts
+# menu option prompts
 def prompt_guests(num):
     guests = []
     i = 0
@@ -35,28 +48,44 @@ def prompt_guests(num):
     return guests
 
 def prompt_checkin():
-    floor = int(input("which floor would you like?: "))
-    room = int(input("which room would you like?: "))
+    floor = input("which floor would you like?: ")
+    room = str(floor) + input("which room would you like?: ")
 
     if check_room_free(floor, room):
         guest_count = int(input("How many guests will be staying?: "))
-        if guest_count > 1:
+        if guest_count < 1:
             print('there needs to be at least one guest!')
             exit
         guests = prompt_guests(guest_count)
         add_guests(floor, room, guests)
 
 def prompt_checkout():
-    print('not implemented yet')
+    room = str(input("what was your room number?: "))
+    floor = str(room[0])
 
+    print('remove: ', floor, room)
+    remove_guests(floor, room)
+
+
+
+# main prompt
 def menu():
-    option = int(input('1. Check in\n2. Check out\n: '))
+    option = int(input('1. Check in\n2. Check out\n3. Dump database\n4. Exit program\n> '))
     if option == 1:
         prompt_checkin()
+
     if option == 2:
         prompt_checkout()
-    else:
-        print('selection must be "1" or "2"')
-        exit
 
-menu()
+    if option == 3:
+        print(db)
+
+    if option == 4:
+        run = False
+
+    if option > 4:
+        print('selection must be one of the available options')
+
+
+# make main loop that calls menu fxn, script style
+while run: menu()
